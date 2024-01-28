59bb51692ed8de3b759c28c23f5178f126844d57
osnofla721 <polazabal0419@gmail.com>
2024-01-28 18:22:04 +0100

    Crear cordenadas acabado, falta insert en bd

--- src/components/creaArea.vue
+++ src/components/creaArea.vue
@@ -31,9 +31,12 @@
 <script>
 import "leaflet/dist/leaflet.css";
 import "leaflet/dist/leaflet.js";
+import "leaflet/dist/leaflet-src.js";
 import L from "leaflet";
 import "leaflet-draw/dist/leaflet.draw.css";
+import "leaflet-draw/dist/leaflet.draw";
 import "leaflet-draw/dist/leaflet.draw-src";
+import "leaflet-draw/dist/leaflet.draw-src.js";
 
 export default {
     data() {
@@ -43,12 +46,12 @@ export default {
             map: null,
             isCreatingArea: false,
             drawnItems: null,
+            drawControl: null,
         };
     },
     methods: {
         // INICIO Y CONFIG DEL MAPA
         initMap() {
-
             this.map = L.map("map").setView([41.38879, 2.15899], 11);
             L.tileLayer(
                 "https://cartodb-basemaps-{s}.global.ssl.fastly.net/rastertiles/voyager/{z}/{x}/{y}.png",
@@ -57,11 +60,11 @@ export default {
                 }
             ).addTo(this.map);
 
-            // Inicializa Dibujos para el mapa
             const drawnItems = new L.FeatureGroup();
             this.map.addLayer(drawnItems);
 
-            const drawControl = new L.Control.Draw({
+            // Crear el control de dibujo y asignarlo a this.drawControl
+            this.drawControl = new L.Control.Draw({
                 edit: {
                     featureGroup: drawnItems,
                 },
@@ -74,67 +77,21 @@ export default {
                     circlemarker: false
                 },
             });
-            this.drawControl = drawControl;
-            this.map.addControl(drawControl);
 
+            this.map.addControl(this.drawControl);
+
+            // Registra el evento L.Draw.Event.CREATED solo una vez aquí
             this.map.on(L.Draw.Event.CREATED, (event) => {
                 const layer = event.layer;
                 drawnItems.addLayer(layer);
 
-                // Obtener las coordenadas en formato GeoJSON
                 const geojson = layer.toGeoJSON();
                 this.saveGeometry(geojson);
-
-                // Desactivar el control de dibujo después de crear un dibujo
-                this.map.off(L.Draw.Event.CREATED);
-            });
-        },
-
-        // CREAR AREA + NOMBRE
-        async crearLugar() {
-            if (this.nombreLugar !== '' && this.drawnGeometries.length > 0) {
-                // Set the flag to indicate that an area creation is in progress
-                this.isCreatingArea = true;
-
-                // Save the area to the database (replace with your actual save method)
-                try {
-                    const areaId = await this.saveAreaToDatabase({
-                        nombre: this.nombreLugar,
-                        geometries: this.drawnGeometries,
-                    });
-
-                    // Display a success message with the area name and ID
-                    alert(`Área creada: ${this.nombreLugar}, ID: ${areaId}`);
-                    console.log(this.drawnGeometries);
-                    // Reset the form and map
-                    this.nombreLugar = '';
-                    this.drawnGeometries = [];
-                    this.limpiarMapa();
-                } catch (error) {
-                    console.error('Error saving area:', error);
-                    alert('Error al guardar el área');
-                }
-
-                this.isCreatingArea = false;
-            }
-        },
-
-        // GUARDAR NOMBRE DE AREA + CORDENADAS (INSERT EN BD)
-        async saveAreaToDatabase(area) {
-            // Simulate an asynchronous API call to save the area and retrieve the ID
-            // Replace this with your actual API call
-            return new Promise((resolve, reject) => {
-                setTimeout(() => {
-                    // Simulate a successful response with the area ID
-                    const areaId = Math.floor(Math.random() * 500) + 1;
-                    resolve(areaId);
-                }, 500);
             });
         },
 
         // LIMPIAMOS MAPA
         limpiarMapa() {
-            // Eliminar las capas dibujadas del mapa
             if (this.map) {
                 this.map.eachLayer((layer) => {
                     if (layer instanceof L.Path || layer instanceof L.Marker) {
@@ -142,24 +99,54 @@ export default {
                     }
                 });
 
-                // Inicializa Dibujos para el mapa
-                const drawnItems = new L.FeatureGroup();
-                this.map.addLayer(drawnItems);
-                this.map.on(L.Draw.Event.CREATED, (event) => {
-                    const layer = event.layer;
-                    drawnItems.addLayer(layer);
-
-                    // Obtener las coordenadas en formato GeoJSON
-                    const geojson = layer.toGeoJSON();
-                    this.saveGeometry(geojson);
-
-                    // Desactivar el control de dibujo después de crear un dibujo
-                    this.map.off(L.Draw.Event.CREATED);
-                });
+                // Reiniciar el valor de drawnGeometries
+                this.drawnGeometries = [];
             }
         },
 
-        // GUARDAS DIBUJOS Y INSERTAR EN LA BD
+        // CREAR AREA + NOMBRE
+        async crearLugar() {
+            if (this.nombreLugar !== '' && this.drawnGeometries.length > 0) {
+                // indicar que se está creando un área
+                this.isCreatingArea = true;
+
+                // Guardar el área en la base de datos (reemplazar con tu método de guardado real)
+                try {
+                    const areaId = await this.saveAreaToDatabase({
+                        nombre: this.nombreLugar,
+                        geometries: this.drawnGeometries,
+                    });
+
+                    // Mostrar un mensaje de éxito con el nombre y ID del área
+                    alert(`Área creada: ${this.nombreLugar}, ID: ${areaId}`);
+                    console.log(this.drawnGeometries);
+                    // Restablecer el formulario y el mapa              
+                    this.nombreLugar = '';
+                    this.drawnGeometries = [];
+
+                } catch (error) {
+                    console.error('Error saving area:', error);
+                    alert('Error al guardar el área');
+                }
+                this.limpiarMapa();
+                this.isCreatingArea = false;
+            }
+        },
+
+        // GUARDAR ID + NOMBRE DE AREA + CORDENADAS (INSERT EN BD)
+        async saveAreaToDatabase(area) {
+            // Simular una llamada asíncrona a la API para guardar el área y obtener el ID
+            // Reemplazar esto con tu llamada API real
+            return new Promise((resolve, reject) => {
+                setTimeout(() => {
+                    // Simular una respuesta exitosa con el ID del área
+                    const areaId = Math.floor(Math.random() * 500) + 1;
+                    resolve(areaId);
+                }, 500);
+            });
+        },
+
+        // GUARDAS DIBUJOS
         saveGeometry(geojson) {
             // Guardar solo las coordenadas en el array
             this.drawnGeometries.push(geojson);
