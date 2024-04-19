558a2dff87361ac88d227ac23ca91683616c4b0e
Pablo Jimenez <a22kevburcac@inspedralbes.cat>
2024-04-19 12:34:40 +0200

    se ven los reportes

--- src/components/incidencias.vue
+++ src/components/incidencias.vue
@@ -86,13 +86,25 @@
 
     <v-dialog v-model="dialogReport" max-width="500">
       <v-card>
-        <v-card-title>Reportes de la incidencia</v-card-title>
-        
+        <v-card-title>Historial de reportes de la incidencia</v-card-title>
+
+        <v-list>
+          <v-list-item v-for="(reporte, index) in reporteSeleccionado" :key="index">
+            <v-list-item-content>
+              <v-list-item-title>{{ reporte.fecha_report }}</v-list-item-title>
+              <v-list-item-subtitle>{{ reporte.mensaje }}</v-list-item-subtitle>
+              <v-list-item-subtitle>{{ reporte.solucionado }}</v-list-item-subtitle>
+
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+
         <v-card-actions>
           <v-btn color="primary" @click="dialogReport = false">Cerrar</v-btn>
         </v-card-actions>
       </v-card>
     </v-dialog>
+
 
     <!-- Diálogo para crear una nueva incidencia -->
     <v-dialog v-model="dialogoVisible" max-width="600px">
@@ -102,7 +114,7 @@
           <v-text-field v-model="nuevaIncidencia.nombre" label="Asunto de la incidencia"></v-text-field>
           <v-textarea v-model="nuevaIncidencia.descripcion" label="Descripción de la incidencia"></v-textarea>
           <v-select v-model="nuevaIncidencia.submarinoSeleccionado" label="Submarino" :items="submarinos.map((submarino) => submarino.nom_sub)
-            " item-text="text" item-value="value"></v-select>
+      " item-text="text" item-value="value"></v-select>
 
           <v-select v-model="nuevaIncidencia.tipo" :items="['Actualización software', 'Reparación']"
             label="Tipo"></v-select>
@@ -139,7 +151,8 @@ import {
   insertIncidencia,
   getIncidencias,
   updateIncidencias,
-  selectIncidenciasEmpresa
+  selectIncidenciasEmpresa,
+  getReports
 } from "@/services/connectionManager.js";
 import { useAppStore } from "@/store/app";
 
@@ -169,6 +182,8 @@ export default {
   data() {
     return {
       listaEnviar: [],
+      reportes: [],
+      reporteSeleccionado: [],
       listaItems: [],
       ordenFecha: {
         campo: null,
@@ -407,11 +422,23 @@ export default {
       this.dialogoEdicionVisible = true;
     },
 
-    reportIncidencia(descripcion) {
-      this.descripcionSeleccionada = descripcion;
+    reportIncidencia(id) {
+      // Encuentra todos los reportes que coinciden con el id_incidencia proporcionado
+      const reportesEncontrados = this.reportes.filter(reporte => reporte.id_incidencia === id);
 
-      this.dialogReport = true;
+      if (reportesEncontrados.length > 0) {
+        // Si se encontraron reportes, asigna el array de reportes encontrados a reporteSeleccionado
+        this.reporteSeleccionado = reportesEncontrados;
+        console.log(this.reporteSeleccionado);
+        // Muestra el diálogo
+        this.dialogReport = true;
+      } else {
+        // Si no se encontró ningún reporte, puedes manejarlo como desees
+        window.alert('No se encontró ningún reporte en esa incidencia.');
+        // Podrías mostrar un mensaje de error o tomar alguna otra acción
+      }
     },
+
 
     // GUARDAR Y HACER UPDATE
     async guardarEdicionIncidencia() {
@@ -519,12 +546,23 @@ export default {
 
       // console.log(this.listaItems);
     },
+
+    async getReportes() {
+      try {
+        this.reportes = await getReports();
+        console.log(this.reportes);
+      } catch (error) {
+        console.error("Error fetching reportes:", error);
+      }
+    }
+
   },
   //CONSOLA
   created() {
     console.log("CREADO");
     this.getSubmarino();
     this.getIncidencia();
+    this.getReportes();
   },
   mounted() {
     console.log("MONTADO");
